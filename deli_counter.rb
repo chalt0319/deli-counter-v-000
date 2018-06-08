dc9fd20a27b26fd8a33ca187dd777b26282cdb18
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-08 15:50:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,8 +14,8 @@ def line(array)
 end 
 
 def take_a_number(array, name)
-  
-  end
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
 def now_serving(array)
