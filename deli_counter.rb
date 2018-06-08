5d908ca41476cf2ab6121c74f26bc1ccaff061ff
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-08 15:14:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(array)
       puts "The line is currently empty."
     else 
       position = array.map.with_index do |person, index|
-        "#{index}. #{person}" 
+        "#{index + 1}. #{person}" 
       end 
       puts "The line is currently: #{position.join(" ")}"
     end
