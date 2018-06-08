aab35e20898e8680e68aa7d1c775043cd9484746
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-08 14:38:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array)
     when 0
       puts "The line is currently empty."
     else 
-      puts "The line is currently: array.join(#{array.length} #{array[array.length]})"
+      puts "The line is currently: array.join("1 ")
     end
 end 
 
