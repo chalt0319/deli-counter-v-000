0b82a5b64627b17c65c6fba7fdbc2b4b47eb7986
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-08 15:26:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ end
 def take_a_number(array, name)
   case array.length
     when 0 
-      array.push(name)
+      array.push("#{name}")
     else 
       array.each do |array|
         array_position = 
