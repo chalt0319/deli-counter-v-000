3a098f576751a14f4ed740ae6ec0ab013c429c06
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-08 14:18:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  case array.length 
+    when 0
+      puts "The line is currently empty."
+    else 
+      puts "The line is currently: #{array.length}. #{array}""
+end 
+
+def take_a_number(array, name)
+  
+  puts "Welcome, #{name}. You are number "
+end
